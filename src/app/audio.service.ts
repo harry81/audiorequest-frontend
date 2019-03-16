487ed09b1c63b3a20e124b9d1ed9ea8d790cf4c9
Hyunmin <pointer81@gmail.com>
2019-03-16 21:26:49 +0900

    Use s3 to keep audio files

--- src/app/audio.service.ts
+++ src/app/audio.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import { environment } from './../environments/environment';
 
@@ -15,19 +15,40 @@ export class AudioService {
     api_path = environment.api_path;
     constructor(private httpClient: HttpClient) {}
 
-    public uploadFile(audio: File): Observable<any> {
-        const formData = new FormData();
+    public uploadFile(presigned_post: any, audio: File): Observable<any> {
+        console.log(typeof(presigned_post));
 
         if (audio) {
-            formData.append('audio', audio);
+            presigned_post.audio = audio;
         }
 
-        return this.httpClient.post(this.api_path + '/stt/', formData);
+        const formData = new FormData();
+        formData.append('AWSAccessKeyId', presigned_post['fields']['AWSAccessKeyId']);
+        formData.append('key', presigned_post['fields']['key']);
+        formData.append('policy', presigned_post['fields']['policy']);
+        formData.append('signature', presigned_post['fields']['signature']);
+
+        // no x-amz-security-token in local env
+        if (presigned_post['fields'].hasOwnProperty('x-amz-security-token')) {
+            formData.append('x-amz-security-token', presigned_post['fields']['x-amz-security-token']);
+        }
+        formData.append('file', audio, audio.name);
+
+        console.log('presigned_post ', formData);
+
+
+        return this.httpClient.post(presigned_post['url'], formData);
     }
 
-    public transcribe(id: string): Observable<any> {
-        const url = this.api_path + '/stt/' + id + '/transcribe/';
-        return this.httpClient.patch(url, '');
+    public transcribe(id: string, key: string, filename: string, to_info: any): Observable<any> {
+        const formData = new FormData();
+        formData.append('id', id);
+        formData.append('key', key);
+        formData.append('email', to_info['email']);
+        formData.append('lang_code', to_info['lang_code']);
+
+        const url = this.api_path + '/stt/';
+        return this.httpClient.post(url, formData);
     }
 
     public get_stt(id: string): Observable<any> {
@@ -42,5 +63,16 @@ export class AudioService {
 
         const url = this.api_path + '/stt/' + id + '/notify/';
         return this.httpClient.patch(url, formData);
+    }
+
+    public presigned_post(filename: string): Observable<any> {
+        console.log('presigned_post is called', filename);
+        const params = new HttpParams().set('filename', filename);
+
+        const url = this.api_path + '/stt/get_presigned_post/';
+
+        console.log(url, params);
+
+        return this.httpClient.get(url, { params: params });
     }
 }
--- src/app/audiorequest/audiorequest.component.ts
+++ src/app/audiorequest/audiorequest.component.ts
@@ -6,12 +6,23 @@ import { NotifyDialogComponent } from '../notify-dialog/notify-dialog.component'
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import { GoogleAnalyticsService} from '../ga.service';
 
+import { DomSanitizer } from '@angular/platform-browser';
 
 /* Status
    - ready
+   - selected
    - wip
    - uploaded
    - sented
+
+* 파일 선택
+ - upload 가능한 url 얻기
+ - 미리 듣기 가능
+
+* 변환
+  - 파일 업로드
+  - 파일형식 변환
+  - 텍스트로 변환
 
 */
 
@@ -28,11 +39,13 @@ export class AudiorequestComponent implements OnInit {
     script = '';
     isSelected = false;
     audiofile = '';
-    path = '';
+    path;
     progress = 'ready';
     stt_id = '';
     email = '';
     response_notify = '';
+    presigned_post: any;
+    file: File;
 
     audioForm = new FormGroup({
         audiofile: new FormControl('', [Validators.required, ]),
@@ -41,7 +54,8 @@ export class AudiorequestComponent implements OnInit {
 
     constructor(private audioService: AudioService,
                 public dialog: MatDialog,
-                public ga: GoogleAnalyticsService) {
+                public ga: GoogleAnalyticsService,
+                private sanitization: DomSanitizer) {
 
     }
 
@@ -60,48 +74,81 @@ export class AudiorequestComponent implements OnInit {
                 this.progress = 'wip';
                 this.audioService.notify(this.stt_id, res).subscribe((response) => {
                     this.response_notify = response.message;
-                    this.progress = 'sent';
                 });
             }
         });
     }
 
-    transcribe() {
-        this.audioService.transcribe(this.stt_id).subscribe(
+    request_transcribe() {
+        /*
+          - 파일 업로드
+            - 파일형식 변환
+            - 텍스트로 변환
+        */
+        this.ga.eventEmitter('audio', 'request_transcribe');
+        const dialogRef = this.dialog.open(NotifyDialogComponent, {
+            width: '250px',
+            data: { email: 'chharry@gmail.com', language: 'ko-KR'}
+        });
+
+        dialogRef.afterClosed().subscribe(to_info => {
+            if (to_info) {
+                this.ga.eventEmitter('audio', 'notify_ready');
+
+                this.progress = 'wip';
+                this.audioService.uploadFile(this.presigned_post, this.file).subscribe(
+                    (res_upload) => {
+                        this.transcribe(to_info);
+                    },
+                    (error) => {
+                        console.log('error upload', error);
+                    }
+                );
+            }
+        });
+    }
+
+    transcribe(to_info: any) {
+        this.audioService.transcribe('10', this.presigned_post['fields']['key'], this.file.name, to_info).subscribe(
             (res) => {
                 this.script = res.script;
+                this.response_notify = res.message;
+
+            },
+            (err) => {},
+            () => {
+                this.progress = 'sent';
             });
     }
 
-    uploadFile($event) {
+    chooseFile($event) {
         this.ga.eventEmitter('audio', 'uploadFile');
         this.clean_page();
 
         const reader = new FileReader();
-        let file: File;
-        this.progress = 'wip';
 
         if ($event.target.files) {
-            file = $event.target.files[0];
+            this.file = $event.target.files[0];
         }
 
-        this.audioService.uploadFile(file).subscribe(
-            (res) => {
-                this.path = res.path;
-                this.stt_id = res.id;
-                this.progress = 'uploaded';
-                this.response_notify = '';
+        const url = URL.createObjectURL(this.file);
+        this.path = this.sanitization.bypassSecurityTrustUrl(url);
+
+        this.audioService.presigned_post(this.file.name).subscribe(
+            (presigned_post) => {
+                this.progress = 'selected';
+                this.presigned_post = presigned_post;
             },
             (err) => {
-                console.log(err.error);
-                this.response_notify = err.error.message;
-                this.progress = 'failed';
+                console.log('error');
             });
+
     }
 
     clean_page() {
         this.response_notify = '';
     }
+
     ngOnInit() {
     }
 
